73fa54c7d2c36128aad6c060b332ce59c28b369e
electr1fy0 <electr1fy0@icloud.com>
2026-05-24 21:06:56 +0530

    feat: setup most things

--- schema.sql
+++ schema.sql
@@ -1,6 +1,12 @@
+drop trigger if exists standups_ai;
+drop trigger if exists standups_au;
+drop trigger if exists standups_ad;
+
+drop table if exists standups_fts;
+drop table if exists standups;
 create table if not exists standups (
     id INTEGER primary key autoincrement,
-    date text not null,
+    date text not null unique,
     morning_plan text,
     evening_done text,
     created_at datetime default current_timestamp
@@ -13,3 +19,39 @@ create virtual table if not exists standups_fts using fts5 (
     content='standups',
     content_rowid='id'
 );
+
+
+create trigger standups_ai after insert on standups begin
+insert into standups_fts(rowid, date, morning_plan, evening_done)
+values (new.id, new.date, new.morning_plan, new.evening_done);
+end;
+
+
+create trigger standups_au after update on standups begin
+insert into standups_fts(standups_fts, rowid, date, morning_plan, evening_done)
+values ('delete', old.id, old.date, old.morning_plan, old.evening_done);
+
+insert into standups_fts(
+rowid, date, morning_plan, evening_done)
+
+
+values (
+new.id, new.date, new.morning_plan, new.evening_done);
+end;
+
+create trigger standups_ad after delete on standups begin
+    insert into standups_fts(
+        standups_fts,
+        rowid,
+        date,
+        morning_plan,
+        evening_done
+    )
+    values (
+        'delete',
+        old.id,
+        old.date,
+        old.morning_plan,
+        old.evening_done
+    );
+end;
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ type Bindings = {
   CHAT_ID: string;
 };
 const app = new Hono<{ Bindings: Bindings }>();
+
 app.get("/cron/morning", async (c) => {
   const redis = Redis.fromEnv(c.env);
   const today = new Date().toISOString().split("T")[0];
@@ -24,10 +25,12 @@ app.get("/cron/morning", async (c) => {
   );
 
   await redis.set(`state:${c.env.CHAT_ID}`, "AWAITING_MORNING", { ex: 7200 });
+  await redis.set(`morning_sent:${today}`, "1", { ex: 60 * 120 });
   return c.json({ ok: true });
 });
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+
+app.get("/ping", (c) => {
+  return c.text("pong");
 });
 
 app.get("/cron/evening", async (c) => {
@@ -42,8 +45,9 @@ app.get("/cron/evening", async (c) => {
     c.env.CHAT_ID,
     "what did you get done today?",
   );
+
   await redis.set(`state:${c.env.CHAT_ID}`, "AWAITING_EVENING", { ex: 7200 });
-  await redis.set(`evening_sent:${today}`, "1", { ex: 86400 });
+  await redis.set(`evening_sent:${today}`, "1", { ex: 60 * 60 * 24 });
 
   return c.json({ ok: true });
 });
@@ -54,16 +58,62 @@ app.get("/meow", async (c) => {
 });
 
 app.post("/webhook", async (c) => {
+  const redis = Redis.fromEnv(c.env);
   const body = await c.req.json();
   const message = body?.message;
   const text = message.text;
   const chatId = message.chat.id.toString() ?? "";
+  const today = new Date().toISOString().split("T")[0];
+  const state = await redis.get(`state:${chatId}`);
+
+  if (text.startsWith("/search")) {
+    console.log("searching...");
+    const query = text.replace("/search", "").trim();
+    const response = await c.env.DB.prepare(
+      `
+      select
+        date,
+        morning_plan,
+        evening_done
+      from standups_fts
+      where standups_fts match ?
+    `,
+    )
+      .bind(query)
+      .all();
+
+    console.log(JSON.stringify(response, null, 2));
+    const results = response.results;
+    if (!results.length) {
+      await handleMessage(c.env.BOT_TOKEN, chatId, "nothing found.");
+    } else {
+      const formatted = results
+        .map((r) => {
+          return `${r.date}\n-> planned: ${r.morning_plan ?? "-"}\n-> done; ${r.evening_done ?? "-"}`;
+        })
+        .join("\n\n");
+      await handleMessage(c.env.BOT_TOKEN, chatId, formatted);
+    }
+
+    return c.json({ ok: true });
+  }
+
+  if (state === "AWAITING_MORNING") {
+    await c.env.DB.prepare(
+      `insert into standups (date, morning_plan) values (?, ?) on conflict(date)
+    do update set morning_plan = excluded.morning_plan`,
+    )
+      .bind(today, text)
+      .run();
+  }
 
   await handleMessage(
     c.env.BOT_TOKEN,
     chatId,
     `hi ${message.chat.first_name}, ${text}`,
   );
+
+  console.log("ok");
   return c.text("ok");
 });
 async function handleMessage(token: string, chatId: string, text: string) {
